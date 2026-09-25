c53e6a611c5d85f2bfe59472b1048f448023497e
jarichardson-collab <jarichardson@davidson.edu>
2026-09-25 19:32:14 -0400

    finished prototype 3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public InputAction jumpAction;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        jumpAction.Enable();
+        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle")){
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            Destroy(dirtParticle.gameObject);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+
+    }
+}
